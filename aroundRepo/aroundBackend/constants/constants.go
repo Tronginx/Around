2ae8f70860a3b72981f51ce80e6ab598f439882b
Tron Tian <96848670+Tronginx@users.noreply.github.com>
2022-08-31 11:19:40 -0400

    Initial commit with working projects

---
+++ aroundRepo/aroundBackend/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.202.0.2:9200"
+	ES_USERNAME = "thx2thz"
+	ES_PASSWORD = "ETian4606226."
+	GCS_BUCKET = "around-bucket-1"
+)
---
+++ aroundRepo/aroundFE/src/constants.js
@@ -0,0 +1,8 @@
+export const TOKEN_KEY = "token";
+export const BASE_URL = "http://34.162.88.202:8080";
+//export const BASE_URL = "https://around-dot-around-179500.uc.r.appspot.com";
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2
+};
